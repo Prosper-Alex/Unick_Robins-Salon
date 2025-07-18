97fa685c3d62f837f05f5824fb92de3b0c822b4c
Prosper Alex <chigoziealex011@gmail.com>
2025-07-18 01:20:38 +0100

    Add Swiper functionality for card content; implement autoplay, pagination, and navigation features

--- script.js
+++ script.js
@@ -18,3 +18,34 @@ window.addEventListener("load", () => {
     },
   });
 });
+
+let swiperCard = new Swiper(".card__content", {
+  loop: true,
+  spaceBetween: 32,
+  grabCursor: true,
+
+  autoplay: {
+    deley: 3000,
+    disableOnInteraction: false,
+  },
+
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+    },
+    968: {
+      slidesPerView: 3,
+    },
+  },
+});
